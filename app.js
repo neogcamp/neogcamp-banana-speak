b348cc6b4e8757d4d6cb07a0d3e67ba8c8fbd4a6
Tanay Pratap <tanay.mit@gmail.com>
2020-11-28 10:38:12 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
-console.log(txtInput)
 
 function clickHandler() {
-    console.log("clicked!");
-    console.log("input", txtInput.value);
+   outputDiv.innerText = "ajsjsjsjsjsjsj  " + txtInput.value;
 };
 
 btnTranslate.addEventListener("click", clickHandler)
-
